b1ef65f46432577fa19d0e0e6ac23a51917e366a
Guille Pérez <guillermopt@outlook.com>
2019-03-30 22:44:22 -0600

    Clase8

--- SimpleExpressionLangauge/simples_expression.cs
+++ SimpleExpressionLangauge/simples_expression.cs
@@ -9,6 +9,7 @@
 
 using System;
 using System.Collections.Generic;
+using System.Text;
 using System.Text.RegularExpressions;
 
 public enum TokenCategory {
@@ -87,42 +88,71 @@ public class Parser {
     }
 
     //Every production gets translated into a method
-    public int Prog() {
-        var result = Exp();
+    public Node Prog() {
+        /* 
+        var n = Exp();
+        n.Add(Exp());
+        */
+        var n = new Prog() {
+            Exp() //implicit call to add
+        };
         Expect(TokenCategory.EOF);
-        return result;
+        return n;
     }
-    public int Exp() {
-        var result = Term();
+    public Node Exp() {
+        var n = Term();
         while(Current == TokenCategory.PLUS) {
-            Expect(TokenCategory.PLUS); // consume +
-            result += Term();
+             var n2 = new Pow() {
+                AnchorToken = Expect(TokenCategory.PLUS)
+            };
+            n2.Add(n);
+            n2.Add(Term());
+            n = n2;
         }
-        return result;
+        return n;
     }
-    public int Term() {
-        var result = Pow();
+
+    //Left Tree
+    public Node Term() {
+        var n = Pow();
         while(Current == TokenCategory.TIMES) {
-            Expect(TokenCategory.TIMES);
-            result *= Pow();
+             var n2 = new Times() {
+                AnchorToken = Expect(TokenCategory.TIMES)
+            };
+            n2.Add(n);
+            n2.Add(Pow());
+            n = n2;
         }
-        return result;
+        return n;
 
     }
-    public int Pow() {
-        var result = Fact();
+
+    //Right Tree
+    public Node Pow() {
+        var n = Fact();
         while(Current == TokenCategory.POW) {
-            Expect(TokenCategory.POW);
-            result = (int) Math.Pow(result, Pow()); //right recursion
+            var n2 = new Pow() {
+                AnchorToken = Expect(TokenCategory.POW)
+            };
+            n2.Add(n);
+            n2.Add(Pow());
+            n = n2;
         }
-        return result;
-        
+        return n;
     }    
-    public int Fact() {
+    public Node Fact() {
             switch (Current){
                 case TokenCategory.INT:
+                    /*
                     var token = Expect(TokenCategory.INT);
-                    return Convert.ToInt32(token.Lexeme);
+                    var node = new Int();
+                    node.AnchorToken = token;
+                    return node;
+                    */
+                    return new Int(){
+                        AnchorToken = Expect(TokenCategory.INT)
+                    };
+                    // return Convert.ToInt32(token.Lexeme);
                 case TokenCategory.PAR_OPEN:
                     Expect(TokenCategory.PAR_OPEN);
                     var result = Exp();
@@ -133,17 +163,160 @@ public class Parser {
             }
         }
 }
+class Node: IEnumerable<Node> {
+
+    IList<Node> children = new List<Node>();
+
+    public Node this[int index] {
+        get {
+            return children[index];
+        }
+    }
+
+    public Token AnchorToken { get; set; }
+
+    //Adds a children node
+    public void Add(Node node) {
+        children.Add(node);
+    }
+
+    public IEnumerator<Node> GetEnumerator() {
+        return children.GetEnumerator();
+    }
+
+    System.Collections.IEnumerator
+    System.Collections.IEnumerable.GetEnumerator() {
+        throw new NotImplementedException();
+    }
+
+    public override string ToString() {
+        return String.Format("{0} {1}", GetType().Name, AnchorToken);
+    }
+
+    public string ToStringTree() {
+        var sb = new StringBuilder();
+        TreeTraversal(this, "", sb);
+        return sb.ToString();
+    }
+
+    static void TreeTraversal(Node node, string indent, StringBuilder sb) {
+        sb.Append(indent);
+        sb.Append(node);
+        sb.Append('\n');
+        foreach (var child in node.children) {
+            TreeTraversal(child, indent + "  ", sb);
+        }
+    }
+}
+
+//Node Classes
+public class Prog: Node { }
+public class Plus: Node { }
+public class Times: Node { }
+public class Pow: Node { }
+public class Int: Node { }
+
+public class EvalVisitor {
+
+    public int Visit (Prog node) {
+        return Visit((dynamic) node[0]);
+    }
+    public int Visit (Plus node) {
+        return Visit((dynamic) node[0]) + Visit((dynamic) node[1]);
+    }
+    public int Visit (Times node) {
+        return Visit((dynamic) node[0]) * Visit((dynamic) node[1]);
+    }
+    public int Visit (Pow node) {
+        return (int) Math.Pow(Visit((dynamic) node[0]), Visit((dynamic) node[1]));
+    }
+    public int Visit (Int node) {
+        return Conver.ToInt32(node.AnchorToken.Lexeme);
+    }
+}
+
+public class CVisitor {
+    public String Visit(Prog node) {
+        var a = Visit((dynamic) node[0]);
+        return @"
+            #include <stdio.h>
+            #include <math.h>
+
+            int main (void) {
+                printf(""%i\n"", "+ a + @");
+            }
+            
+            return 0;
+            ";
+    }
+    public String Visit(Plus node) {
+        var a = Visit((dynamic) node[0]);
+        var b = Visit((dynamic) node[1]);
+        return $"({a}+{b})";
+    }
+    public String Visit(Times node) {
+        var a = Visit((dynamic) node[0]);
+        var b = Visit((dynamic) node[1]);
+        return $"({a}*{b})";
+    }
+    public String Visit(Pow node) {
+        var a = Visit((dynamic) node[0]);
+        var b = Visit((dynamic) node[1]);
+        return $"({a},{b})";
+    }
+    public Stinrg Visit(Int node) {
+        return node.AnchorToken.Lexeme;
+    }
+    
+}
+
+public class LispVisitor {
+    public String Visit(Prog node){
+        return @"
+            (require '[clojure.math.numeric-tower : refer [expt]])" + Visit((dynamic) node[0]);
+
+    }
+
+    public String Visit(Plus node){
+        var a = Visit((dynamic) node[0]);
+        var b = Visit((dynamic) node[1]);
+        return $"(+ {a} {b})";
+    }
+    public String Visit(Times node){
+        var a = Visit((dynamic) node[0]);
+        var b = Visit((dynamic) node[1]);
+        return $"(* {a} {b})";
+    }
+    public String Visit(Pow node){
+        var a = Visit((dynamic) node[0]);
+        var b = Visit((dynamic) node[1]);
+        return $"(expt {a} {b})";
+    }
+    public String Visit(Int node){
+        return node.AnchorToken.Lexeme;
+    }
+}
 public class Driver { 
     public static void Main() {
         Console.Write("> ");
         var line = Console.ReadLine();
         var parser = new Parser(new Scanner(line).Start().GetEnumerator());
         try {
+            var ast = parser.Prog();
+            // var result = new EvalVisitor().Visit((dynamic) ast);
+            // programa en c var result = new CVisitor().Visit((dynamic) ast);
+            //programa en lisp
+            Console.WriteLine(result);
+        } catch (SyntaxError) {
+            Console.WriteLine("SyntaxErro");
+        }
+        
+        /*try {
             var result = parser.Prog();
             Console.Write(result);
         } catch (SyntaxError) {
             Console.WriteLine("Valio verga su syntax del mameitor");
-        }
+        }*/
         /* foreach (var token in new Scanner(line).Start()) {
             Console.WriteLine(token);
         }
