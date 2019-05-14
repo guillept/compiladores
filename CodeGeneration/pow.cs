2a1804d85478c6205d58366f086928b6543dc6f4
Guille Pérez <guillermopt@outlook.com>
2019-05-13 23:24:10 -0500

    el compilador ya compilando

--- CodeGeneration/pow.cs
+++ CodeGeneration/pow.cs
@@ -5,4 +5,9 @@ class Test {
         int X = (int) Math.Pow(5 ,3);
         Console.WriteLine(X);
     }
+
+    public static void MainDos() {
+        int X = (int) Math.Pow(5 ,3);
+        Console.WriteLine(X);
+    }
 }
